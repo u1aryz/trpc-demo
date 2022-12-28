3eb343109c5cb302550e4fdbce31c29d8a8902cf
u1aryz <u1aryz.d@gmail.com>
2022-12-29 01:07:18 +0900

    eslint-plugin-prettierを導入

--- packages/eslint-config-custom/index.js
+++ packages/eslint-config-custom/index.js
@@ -1,7 +1,19 @@
 module.exports = {
-  extends: ["next", "turbo", "prettier"],
+  plugins: ['prettier'],
+  extends: ['next', 'turbo', 'prettier'],
   rules: {
-    "@next/next/no-html-link-for-pages": "off",
-    "react/jsx-key": "off",
+    '@next/next/no-html-link-for-pages': 'off',
+    'react/jsx-key': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'all',
+        bracketSpacing: true,
+        arrowParens: 'always',
+        printWidth: 120,
+        singleQuote: true,
+        semi: false,
+      },
+    ],
   },
-};
+}
